ee02e16c7d3d618daa40dba4fc80d3cac0d247be
hao0609 <cnblue603@gmail.com>
2024-12-30 17:57:55 +0800

    first commit

---
+++ vue-project/src_Test/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+
+</script>
+
+
+<style>
+    @font-face {
+        font-family: 'Dela Gothic One';
+        src: url('/src/assets/font/DelaGothicOne-Regular.ttf') format('truetype');
+
+        font-family: 'NotoSansTC';
+        src: url('/src/assets/font/NotoSansTC-VariableFont_wght') format('truetype');
+    }
+
+
+    @media screen {
+        
+    }
+</style>
+
+<template>
+    <Navbar/>
+    <div class="wrapper">
+    </div>
+
+</template>
---
+++ vue-project/src_Test/components/Navbar.vue
@@ -0,0 +1,92 @@
+
+<script setup>
+import icon from './icons/icon.vue' 
+import shopping_cart from './icons/shopping_cart.vue' 
+import user from './icons/user.vue' 
+
+</script>
+
+
+<template>
+    <header>
+        <div class="icon">
+            <icon/>
+            <div class="icon_text">
+                METRO
+            </div>      
+        </div>            
+        <nav>
+            <ul class="menu">
+                <li><RouterLink to="/" >景點介紹</RouterLink></li>
+                <li><RouterLink to="/">積分任務</RouterLink></li>
+                <li><RouterLink to="/">商城</RouterLink></li>
+                <li><RouterLink to="/">最新消息</RouterLink></li>
+            </ul>
+
+            <div class="line"></div>
+
+            <div class="shopping_cart"><shopping_cart/></div> 
+            <div class="user"><user/></div> 
+
+            
+        </nav>
+
+    </header>
+
+
+
+</template>
+
+<style>
+ .icon_text{
+    font-family: 'Dela Gothic One';
+    font-size: 24px;
+    color: white;
+ }
+ .icon{
+    display: flex;
+    gap: 15px;
+    align-items: center;
+ }
+ header{
+    display: flex;
+    width: 100%;
+    background-color: black;
+    height: 72px;
+    /* position: absolute; */
+    padding: 0px 40px;
+    justify-content:space-between
+
+
+ }
+ ul{
+    list-style: none;
+    display: flex;
+ }
+ nav{
+    display: flex;
+    align-items: center;
+ }
+.menu{
+    font-size: 16px;
+    font-family: 'NotoSansTC';
+    display: flex;
+    padding: 0;
+    gap: 32px;
+    color: white;
+ }
+ .line{
+    width: 1px;
+    height: 21px;
+    background: white;
+    margin: 0 36px;
+ }
+ .shopping_cart{
+    margin-right: 36px;
+ }
+
+
+ 
+
+</style>
+
